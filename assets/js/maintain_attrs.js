93877561098025fbbf7e695961342a6647dbf188
shibao <shibao@bubbletea.dev>
2022-01-23 00:00:53 -0500

    mix format

--- assets/js/maintain_attrs.js
+++ assets/js/maintain_attrs.js
@@ -2,7 +2,7 @@
 // update. https://github.com/phoenixframework/phoenix_live_view/issues/1011
 
 export default {
-  attrs(){ return this.el.getAttribute("data-attrs").split(", ") },
-  beforeUpdate(){ this.prevAttrs = this.attrs().map(name => [name, this.el.getAttribute(name)]) },
-  updated(){ this.prevAttrs.forEach(([name, val]) => this.el.setAttribute(name, val)) }
+  attrs () { return this.el.getAttribute('data-attrs').split(', ') },
+  beforeUpdate () { this.prevAttrs = this.attrs().map(name => [name, this.el.getAttribute(name)]) },
+  updated () { this.prevAttrs.forEach(([name, val]) => this.el.setAttribute(name, val)) }
 }
