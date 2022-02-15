fc5b03d6809dfbb77a8b5caa37a442960b0073a5
shibao <shibao@bubbletea.dev>
2022-02-15 18:57:01 -0500

    enable live dashboard os monitoring

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule Lokal.MixProject do
   def application do
     [
       mod: {Lokal.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :os_mon]
     ]
   end
 
