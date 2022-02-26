41090c46d031a7b78b149036030aa96a3a11c9b9
shibao <shibao@bubbletea.dev>
2022-02-25 21:52:17 -0500

    add invites

---
+++ priv/repo/migrations/20210904211727_create_invites.exs
@@ -0,0 +1,19 @@
+defmodule Lokal.Repo.Migrations.CreateInvites do
+  use Ecto.Migration
+
+  def change do
+    create table(:invites, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+      add :name, :string
+      add :token, :string
+      add :uses_left, :integer, default: nil
+      add :disabled_at, :naive_datetime, default: nil
+
+      add :user_id, references(:users, on_delete: :delete_all, type: :binary_id)
+
+      timestamps()
+    end
+
+    create index(:invites, [:user_id])
+  end
+end
