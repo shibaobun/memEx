91794ddc5503e52e605e4ef093443a45073c4919
shibao <shibao@bubbletea.dev>
2023-01-22 22:36:20 -0500

    upgrade dependencies

--- mix.exs
+++ mix.exs
@@ -5,9 +5,9 @@ defmodule Lokal.MixProject do
     [
       app: :lokal,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
-      compilers: [:gettext] ++ Mix.compilers(),
+      compilers: Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps(),
