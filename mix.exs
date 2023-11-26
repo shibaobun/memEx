7189c955c39933e6bba92081fcdcd405087dba71
shibao <shibao@bubbletea.dev>
2023-11-26 17:30:20 -0500

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Memex.MixProject do
   def project do
     [
       app: :memex,
-      version: "0.1.14",
+      version: "0.1.15",
       elixir: "1.15.6",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
